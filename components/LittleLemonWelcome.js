5b15a1a7a62dc18100e107d88e5013f2e8a4e259
MenaceHecker <thedynamic2508@gmai.com>
2025-05-27 02:55:23 -0400

    First Commit

---
+++ components/LittleLemonWelcome.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+export default function LittleLemonHeader() {
+  return (
+   <View style={{backgroundColor: '#505c5c', flex: 1}}>
+        <Text style={{fontSize: 40, color: 'white', textAlign: 'center', padding: 10, fontFamily: 'Roboto'}} numberOfLines={2} >
+        Welcome to Little Lemon
+        </Text>
+        <Text style={{fontSize: 28, marginVertical: 8, color: 'white', padding: 12, textAlign: 'center', fontFamily: 'sans-serif'}}>
+        Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails in a lively but casual environment. We would love to hear more about your experience with us!
+        </Text>
+   </View> 
+  );
+}
